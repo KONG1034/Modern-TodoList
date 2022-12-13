679694b0af556190002c7ac1f1110d692fe032e6
KONG1034 <kyh93054@naver.com>
2022-12-14 01:47:38 +0900

    Kong - 모든 페이지 기본적인 구조 및 스타일 구현

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,9 +5,16 @@ export const Todo = () => {
         window.confirm('정말 삭제 하겠습니까?');
     }
     const updateHandler = () => {
+
+    }
+    const createHandler = () => {
         
     }
     return <>
+        <div className="todo-div">
+            <div className="todo-input-div"><input type="text" id="todo" name="todo" placeholder="해야할 일을 추가해주세요."/></div>
+            <button onClick={createHandler}>추가</button>
+        </div>
         <ul>
             <li>Todo item <button onClick={updateHandler}>수정</button><button onClick={deleteHandler}>삭제</button></li>
         </ul>
